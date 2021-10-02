a1acaa8aa2759c44522b46d102ccc0532dea95ec
vershardRahming <vershardrahming2@gmail.com>
2021-10-02 18:06:37 -0500

    Map2 added

--- settings.py
+++ settings.py
@@ -10,13 +10,13 @@ BLUE = (0, 0, 255)
 ORANGE = (255, 165, 0)
 
 # game settings
-WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
-HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+WIDTH = 512   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 384  # 16 * 48 or 32 * 24 or 64 * 12
 FPS = 60
 TITLE = "Accountability Partner Tower Defense"
 BGCOLOR = DARKGREY
 
-TILESIZE = 32
+TILESIZE = 16
 GRIDWIDTH = WIDTH / TILESIZE
 GRIDHEIGHT = HEIGHT / TILESIZE
 
