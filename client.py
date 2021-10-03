c9ddd9c170efe6d6a56e86967d9c7c934bcc5e19
Surya <46317327+bond1999@users.noreply.github.com>
2021-10-03 03:53:29 -0500

    Enemy Running Loop Created

--- client.py
+++ client.py
@@ -1,11 +1,29 @@
 import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 5000))
+HEADER = 64
+PORT = 8008
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.3.147"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    thread = threading.Thread(target=recv)
+    thread.start()
+
+def recv():
+    while True:
+        print(client.recv(2048).decode(FORMAT))
 
 while True:
-    msg = s.recv(24)
-    print(msg.decode("utf-8"))
-
-
-
+    send(input())
