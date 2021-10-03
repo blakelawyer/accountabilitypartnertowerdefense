fd7416c4f00a74013e48f9484960c11856bdbfd7
Surya <46317327+bond1999@users.noreply.github.com>
2021-10-03 03:56:35 -0500

    Enemy Running Loop Created

--- server.py
+++ server.py
@@ -1,20 +1,38 @@
 import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 5000))
+HEADER = 64
+PORT = 8008
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-s.listen(5)
-
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established")
-    clientsocket.send(bytes("Welcome to the server", "utf-8"))
-
-
-
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 
+def handle_client(conn, addr):
+    thread = threading.Thread(target=send, args=conn)
+    thread.start()
+    while True:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            print(f"[{addr}] {msg}")
 
 
+def send(conn):
+    while True:
+        conn.send(input().encode(FORMAT))
 
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
 
+print("[STARTING] server is starting...")
+start()
