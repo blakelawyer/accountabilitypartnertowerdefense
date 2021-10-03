56a801de0a36f598b5085a4c69b6705dc243ab30
blakelawyer <blakethelawyer@gmail.com>
2021-10-03 09:02:34 -0500

    Getting on the same page.

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ HIGHLIGHT = (102, 255, 255)
 # game settings
 WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
 HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
-FPS = 45
+FPS = 60
 TITLE = "Accountability Partner Tower Defense"
 BGCOLOR = DARKGREY
 
